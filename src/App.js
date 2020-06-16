8d1993e381bc3adeb2fe0511947a24b09661e150
skyraider <akashyadav@gmail.com>
2020-06-16 10:19:50 +0530

    Added elements for apis like image, details, etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
+
+import Loader from './loader';
 
 function App() {
+  const [place, setPlace] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      // await fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
+      await fetch('http://node.uttarakhandtraveller.com/places/1', {
+        // mode: 'cors',
+        // crossDomain: true,
+        // headers: {},
+      })
+        .then(resp => resp.json())
+        .then(res => {
+          setPlace(res.place);
+          setIsLoading(false);
+          console.log(res, isLoading);
+          console.log(isLoading);
+        })
+        .catch(error => console.log(error));
+    };
+    fetchData();
+  }, [isLoading]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="place__container">
+          <img src={place.image} alt={place.name} className="place__image" />
+          <details className="place__details  ">
+            <summary>
+              <span className="place__location">
+                Location:{place.location.slice(2)}
+              </span>
+              <span className="place__name">Name:{place.name}</span>
+            </summary>
+            {place.detail}
+          </details>
+          <a href={place.place_url} className="place__link">
+            More details
+          </a>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="spinner-box">
+    <div className="circle-border">
+      <div className="circle-core"></div>
+      loading
+    </div>
+  </div>
+);
