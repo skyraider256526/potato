dd47d88a88c9b355199304e7e8e2be3a531ef86d
skyraider <akashyadav@gmail.com>
2020-06-16 10:36:48 +0530

    Updates

---
+++ precache-manifest.941afb20c4219711073e68312de1ee06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fac9a33c0843d5798599d0ca54ac33",
+    "url": "/potato/index.html"
+  },
+  {
+    "revision": "3eea8af6161792dc7604",
+    "url": "/potato/static/css/main.f6bf858b.chunk.css"
+  },
+  {
+    "revision": "aef3f3e37f2b09ef2d1b",
+    "url": "/potato/static/js/2.e91b7785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/potato/static/js/2.e91b7785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eea8af6161792dc7604",
+    "url": "/potato/static/js/main.568b266d.chunk.js"
+  },
+  {
+    "revision": "44dc1d53229b06d6ef6c",
+    "url": "/potato/static/js/runtime-main.53aa6ea8.js"
+  }
+]);
